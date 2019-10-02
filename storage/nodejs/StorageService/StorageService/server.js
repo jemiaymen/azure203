c9161bf68efe6807f63226bc0998171f3d51476c
jemi aymen (Jemix) <jemiaymen@gmail.com>
2019-10-02 22:15:47 +0100

    finish storage staff

--- storage/nodejs/StorageService/StorageService/server.js
+++ storage/nodejs/StorageService/StorageService/server.js
@@ -1,16 +1,13 @@
 'use strict';
-var http = require('http');
-var port = process.env.PORT || 3000;
 
-const path = require('path');
 const storage = require('azure-storage');
 const service = storage.createBlobService('DefaultEndpointsProtocol=https;AccountName=data203;AccountKey=wuYWMO76VzTuTI2I5aRzCABPtUFlLpRGg+Xgc7sLpDxObcauuz3yW/SVY329+dMTKOxrvAFupk1g+zgFP9056A==;EndpointSuffix=core.windows.net');
-
 const blobName = 'blob1';
 const containerName = 'container';
+const fileName = 'data.txt';
 
 
-const download = (containerName, blobName) => {
+const getBlob = (containerName, blobName) => {
     service.getBlobToText(containerName, blobName, (err, data) => {
         if (err)
             console.log(err);
@@ -28,5 +25,20 @@ const deleteBlob = (containerName, blobName) => {
     });
 }
 
-//download(containerName, blobName);
-deleteBlob(containerName, blobName);
+const createBlob = (containerName,blobName,fileName) => {
+    service.createContainerIfNotExists(containerName,err => {
+        if(err)
+            console.log(err);
+    });
+
+    service.createPageBlobFromLocalFile(containerName,blobName,fileName,(err,result) =>{
+        if(err)
+            console.log(err);
+        else
+            console.dir(result, { depth: null, colors: true });
+    });
+}
+
+createBlob(containerName,blobName,fileName);
+//getBlob(containerName, blobName);
+//deleteBlob(containerName, blobName);
